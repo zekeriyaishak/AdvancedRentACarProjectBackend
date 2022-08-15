9461746a784695d2dd613905493f49043b7e9409
zekishak <zekishakdgny38@gmail.com>
2022-08-16 00:05:41 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Application/Services/Repositories/IBrandRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Repositories
+{
+    //Brand'e özel operasyonlar buraya yazılacak
+    //onion architecture - app katmanında unit test yazılır. asla Data Access'e gidilmez
+    public interface IBrandRepository: IAsyncRepository<Brand>, IRepository<Brand>
+    {
+    }
+}
